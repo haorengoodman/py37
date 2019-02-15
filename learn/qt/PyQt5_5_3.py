ab1d651026ad3d7889b3a0db9d84e86866e33232
gaotao <gaotao@news.cn>
2019-02-15 16:12:19 +0800

    剪刀石头布小游戏 自定义信号

---
+++ learn/qt/PyQt5_5_3.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+# @File    : PyQt5_5_3.py
+# @Date    : 2019-02-15
+# @Author  : gaotao
+from random import randint
+import sys
+
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QMessageBox
+
+
+# 剪刀、石头、布
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        # 新建三个按钮
+        self.bt1 = QPushButton("石头",self)
+        self.bt2 = QPushButton("剪刀",self)
+        self.bt3 = QPushButton("布",self)
+        self.initUI()
+
+    def initUI(self):
+        # 设置窗口
+        self.setWindowTitle("信号、槽")
+        self.setGeometry(200, 200, 500, 300)
+        # 设置按钮属性
+        self.bt1.setGeometry(115,200,70,40)
+        self.bt2.setGeometry(215,200,70,40)
+        self.bt3.setGeometry(315,200,70,40)
+        # 事件与槽位关联
+        self.bt1.clicked.connect(self.buttonClick)
+        self.bt2.clicked.connect(self.buttonClick)
+        self.bt3.clicked.connect(self.buttonClick)
+
+        # 窗口显示
+        self.show()
+
+    def buttonClick(self):
+        # 1:石头 2:剪刀 3:布
+        guess_num = randint(1,3)
+        sender = self.sender()
+        btn_text = sender.text()
+        if btn_text == "石头":
+            if guess_num == 1:
+                QMessageBox.about(self,"石头","平手")
+            elif guess_num == 2:
+                QMessageBox.about(self,"剪刀","赢了")
+            elif guess_num == 3:
+                QMessageBox.about(self,"布","输了")
+        if btn_text == "剪刀":
+            if guess_num == 1:
+                QMessageBox.about(self,"石头","输了")
+            elif guess_num == 2:
+                QMessageBox.about(self,"剪刀","平手")
+            elif guess_num == 3:
+                QMessageBox.about(self,"布","赢了")
+        if btn_text == "布":
+            if guess_num == 1:
+                QMessageBox.about(self,"石头","赢了")
+            elif guess_num == 2:
+                QMessageBox.about(self,"剪刀","输了")
+            elif guess_num == 3:
+                QMessageBox.about(self,"布","平手")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    example = Example()
+    sys.exit(app.exec_())
+
---
+++ learn/qt/PyQt5_5_4.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+# @File    : PyQt5_5_3.py
+# @Date    : 2019-02-15
+# @Author  : gaotao
+import sys
+from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtGui import QPainter
+
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMessageBox
+
+
+# 自定义信号源
+class Signal(QObject):
+    showmouse = pyqtSignal()
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.s = Signal()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle('学点编程吧')
+        # 信号与槽位关联
+        self.s.showmouse.connect(self.about) # 信号和槽位关联
+        self.show()
+
+    def about(self):
+        QMessageBox.about(self,'鼠标','你点鼠标了吧！')
+
+    def mousePressEvent(self, e):
+        self.s.showmouse.emit() # emit 发射信号
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
