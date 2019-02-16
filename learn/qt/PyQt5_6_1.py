02e1ba7a5b15097e04b97d113672b8eb0793e982
haorengoodman@163.com <P@ssw0rd>
2019-02-16 17:15:40 +0800

    自定义布局  拉伸因子 stretch，通过拉伸因子来控制 指定空白 被切分为多少等分

--- learn/qt/PyQt5_6_1.py
+++ learn/qt/PyQt5_6_1.py
@@ -2,8 +2,8 @@
 
 # -*- coding: utf-8 -*-
 
-# @File    : PyQt5_6_0.py
-# @Date    : 2019-02-15
+# @File    : PyQt5_6_1.py
+# @Date    : 2019-02-16
 # @Author  : gaotao
 import sys
 from PyQt5.QtCore import QObject, pyqtSignal
@@ -12,7 +12,7 @@ from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMessageBox, QPushButton, QHBoxLayout, QVBoxLayout
 
 
-# 自定义  水平布局、垂直布局
+# 自定义布局  拉伸因子 stretch，通过拉伸因子来控制 指定空白 被切分为多少等分
 class Example(QWidget):
 
     def __init__(self):
@@ -24,18 +24,25 @@ class Example(QWidget):
 
     def initUI(self):
         # 设置窗口属性
-        self.setGeometry(200, 200, 300, 300)
+        self.setGeometry(200, 200, 500, 350)
         self.setWindowTitle('自定义布局')
         # 设置窗口布局
-        hbox = QHBoxLayout()  # 水平布局
-        hbox.addStretch(1)  # 添加拉伸因子
+        # 水平方向的空白被分成了6等份儿
+        hbox = QHBoxLayout()
+        hbox.addStretch(2)
         hbox.addWidget(self.bt1)
+        hbox.addStretch(1)
         hbox.addWidget(self.bt2)
+        hbox.addStretch(1)
         hbox.addWidget(self.bt3)
-        vbox = QVBoxLayout()  # 垂直布局
-        vbox.addStretch(1)  # 添加拉伸因子
+        hbox.addStretch(2)
+        # 垂直布局
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
         vbox.addLayout(hbox)
-        self.setLayout(vbox)  # 使用布局
+        # 将布局添加到窗口
+        self.setLayout(vbox)
+
         self.show()
 
 
