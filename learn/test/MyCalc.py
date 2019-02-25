947221e86925211cf27071a90dc6cd42b939d8de
gaotao <gaotao@news.cn>
2019-02-25 16:33:49 +0800

    测试文件内容提交

---
+++ learn/test/MyCalc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/25 16:32
+# @Author  : journal
+# @File    : MyCalc.py
+# @Software: PyCharm
+
+__author__ = 'journal'
+
+
+
---
+++ learn/test/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/25 9:41
+# @Author  : journal
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+__author__ = 'journal'
+
